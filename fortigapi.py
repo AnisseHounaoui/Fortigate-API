197994ec3798674c3131f01ed0d1f6080d51b3a0
anisse.hounaoui <protozik10012@gmail.com>
2025-01-10 23:31:44 -0500

    Fetch for device infos and get HA sync config

--- fortigapi.py
+++ fortigapi.py
@@ -20,30 +20,42 @@ def save_cookies(session,cookie_file):
 def load_cookies(session, cookie_file, fg_url):
     if os.path.exists(cookie_file):
         with open(cookie_file, 'rb') as f:
+            # case 1 : cookies exists and expired or doesnt exists in file -> test url and results doesnt exists -> return None (to initial new login for new cookies in logincheck)
+            # case 2: cookies exists and valid -> test  url and verify that results exists -> return session.cookies
             cookies = pickle.load(f)
-
-        #verify that cookies exists and valid with test GET
+            f.close()
+        # verify that cookies exists and valid with test GET
         test_url = fg_url + "/api/v2/monitor/system/ha-checksums?vdom=root"
         headers = {
             'Content-Type': 'application/json'
         }
         session.cookies.update(cookies)
-        response = requests.request("GET", test_url, headers=headers, cookies=session.cookies, verify=False)
+        response = requests.get(test_url, headers=headers, cookies=session.cookies, verify=False)
+        try:
+            print(f"test response: {response.json()}")
+            if response.json():  # verifying that response content is returned = cookies are valid
 
-        print(f"test response: {response.json()['results']}")
-        if response.json()['results']: #verifying that response content is returned = cookies are valid
-            return session.cookies
+                return session.cookies
+            else:
+                print("No results found in response.")
+                return None
+        except requests.exceptions.JSONDecodeError:
+            print(f"Error decoding JSON response from {test_url}: {response.text}")
+            return None
+    else:
         return None
 
 
-def logincheck(fortitoken, config):
 
-    fg_url = config["fg_url"]
+def logincheck(config):
+
+    fg_url = config["fg_url55"]
     fg_url_login = fg_url + "/logincheck"
     session = requests.session()
 
     cookie_file = f"{fg_url.split('//')[1].split(':')[0]}_cookies.pkl"
     cookies = load_cookies(session, cookie_file, fg_url)
+
     print(f"Cookies loaded: {cookies}")
 
     # Invalid or missing (if first time) cookies, initial login
@@ -60,6 +72,7 @@ def logincheck(fortitoken, config):
 
         response1 = session.post(fg_url_login, headers=headers, data=payload, verify=False)
         session.cookies.update(response1.cookies)
+        fortitoken = int(input("Enter fortitoken:"))
         payload["token_code"] = fortitoken
         response2 = session.post(fg_url_login, headers=headers, cookies=session.cookies, data=payload, verify=False)
 
@@ -71,14 +84,60 @@ def logincheck(fortitoken, config):
         session.cookies = cookies
 
     print(f"Final cookies: {session.cookies}")
-    #return session.cookies
+    return session.cookies
+
+# FG version
+def get_info(fg_url, cookies):
+    ha_url = fg_url + "/api/v2/monitor/system/firmware?vdom=root"
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("GET", ha_url, headers=headers, cookies=cookies, verify=False)
+
+    #name = response.json()['results']
+    serial_num = response.json()['serial']
+    version = response.json()['version']
+    build = response.json()['build']
+
+    print(f"\nDevice infos: {fg_url}")
+    print(f"\nSerial Number: {serial_num}")
+    print(f"\nVersion: {version} build {build}")
+
+# Fetch HA status
+def checksum_compare(result):
+    checksum_primary = result[0]["checksum"]["all"]
+    checksum_secondary = result[1]["checksum"]["all"]
+    return checksum_primary == checksum_secondary
+def get_ha(fg_url, cookies):
+    ha_url = fg_url + "/api/v2/monitor/system/ha-checksums?vdom=root"
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("GET", ha_url, headers=headers, cookies=cookies, verify=False)
+    results = response.json()['results']
+    if checksum_compare(results):
+        print("Fortigates are Synchronized")
+    else:
+        print("Fortigates are NOT Synchronized")
+
+
+def get_config(config):
+    fg_cookies = logincheck(config)
+    fg_url = config["fg_url55"]
+    print(f"\nFetching configuration for {fg_url}")
+    print(get_info(fg_url,fg_cookies))
+    print(get_ha(fg_url,fg_cookies))
+
 
 def main():
 
     config = load_config()
     #fg_url  = config["fg_url"]
-    fortitoken = int(input("Enter fortitoken:"))
-    print(logincheck(fortitoken,config))
+    #fortitoken = int(input("Enter fortitoken:"))
+
+    print(get_config(config))
 
 if __name__ == '__main__':
     main()
